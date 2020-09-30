70e627426bcfa321c0eb2275b696473de6cd6c3b
dtn75570 <71202757+dtn75570@users.noreply.github.com>
2020-09-30 15:05:58 -0500

    We finished the programs
    We finished the programs

--- forms/WhileAB/WhileAB.js
+++ forms/WhileAB/WhileAB.js
@@ -1,21 +1,16 @@
-/*Use 2 while loops. 
-Get as many cities from the user as they want - stop getting cities when the user says to stop. 
-The user can use any case mixture in their input (eg. SAN FranciSCO)
-Cities go into an array named 'cities'.  
-When city input is done, use a while loop that outputs each city on its' own line in all lower case.
-The output must be to the console, in this format:
 
-eg. say the user inputs "OmaHa" and "St. LOUIS"
-*/
-
-cityArray = []
-cityInput = prompt('Please enter a city')
+//Get as many cities from the user as they want - stop getting cities when the user says to stop. 
+let cityArray = []
+let cityInput = prompt('Please enter a city')
 while (cityInput != ''){
+//Cities go into an array named 'cities'. 
     cityArray.push(cityInput)
     cityInput = prompt('Please enter a city, or nothing to end the input.')
 }
 i = 0
 while (i < cityArray.length){
+  
+//When city input is done, use a while loop that outputs each city on its' own line in all lower case.
     console.log(cityArray[i].toLowerCase())
     i += 1
 }

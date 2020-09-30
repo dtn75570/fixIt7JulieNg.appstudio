800db3ff2b01569cf20b7ec5cfeed3859918fb6f
dtn75570 <71202757+dtn75570@users.noreply.github.com>
2020-09-29 23:46:38 -0500

    I created the code for the second program
    I created the code for the second program

---
+++ forms/forAB/forAB.js
@@ -0,0 +1,23 @@
+/*Create a new form named 'forAB.
+Create an array named 'names' that holds this data:
+
+Bob, Janet, Tom, Bob, Randy, Elizabeth, Kim, Nancy
+Each time the loop runs, it must output a name. So eventually your program will have output each name, one name per line.
+The output must be to the console, with the first line showing the last name in the array, then the second line showing the second to last name in the array, etc. until the last name output will be the one that was first in the array. Basically in reverse order (there are several ways to do this - choose any one).
+Use this format:
+
+Nancy
+Kim
+Elizabeth
+Randy
+Bob
+Tom
+Janet
+Bob
+*/ 
+
+names = ['Bob', 'Janet', 'Tom', 'Bob', 'Randy', 'Elizabeth', 'Kim', 'Nancy']
+i = 0
+for (i = 0; i < names.length; i++){
+    console.log(names[i])
+}
